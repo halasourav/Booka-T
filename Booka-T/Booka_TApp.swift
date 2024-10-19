1d824daa8ce03b7e0f68988c7ff87739274653e0
Sourav Bhattacharjee <sourav.bhattacharjee78@gmail.com>
2024-10-19 10:28:31 +0530

    Initial Commit

---
+++ Booka-T/Booka_TApp.swift
@@ -0,0 +1,17 @@
+//
+//  Booka_TApp.swift
+//  Booka-T
+//
+//  Created by Sourav Bhattacharjee on 19/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Booka_TApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
